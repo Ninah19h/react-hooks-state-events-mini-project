67deee40b2614db2eb1c2c02f05d16f4b3f4c703
Ninah19h <jerolinaha@gmail.com>
2025-08-24 20:38:15 +0300

    state and events

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Task() {
+function Task({ text, category, onDeleteTask }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  function handleClick() {
+    if (onDeleteTask) {
+      onDeleteTask();
+    }
+    setIsVisible(false);
+  }
+
+  if (!isVisible) return null;
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onDeleteTask }) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((t, index) => (
+        <Task
+          key={index}
+          text={t.text}
+          category={t.category}
+          onDeleteTask={() => onDeleteTask(t)}
+        />
+      ))}
     </div>
   );
 }
